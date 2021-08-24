77f28d08326fe911018da90860dbdbe8ca41eae7
unknown <zenfonezd551@gmail.com>
2021-08-24 20:50:53 +0300

    modified app.js, and main-products.scss

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const like = document.querySelectorAll('.main__products-like');
-for (let i of like) {
-    i.addEventListener('click', () => {
-       i.classList.toggle('like-red');
-    })
-}
+const likeBtn = document.querySelectorAll('.main__products-like');
+
+likeBtn.forEach(function(item) {
+    item.addEventListener('click', () =>
+        item.classList.toggle('main__products-like_clicked'));
+});
