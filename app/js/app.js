594ec16b39825aa413907c0b2b9924d9b83fa2e0
unknown <zenfonezd551@gmail.com>
2021-08-25 20:09:37 +0300

    feat: add swiper for index.html

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,27 @@
 'use strict';
 
 const likeBtn = document.querySelectorAll('.main__products-like');
+const likeBtn2 = document.querySelectorAll('.catalog__products-like');
 
 likeBtn.forEach(function(item) {
     item.addEventListener('click', () =>
         item.classList.toggle('main__products-like_clicked'));
 });
+
+likeBtn2.forEach(function(item) {
+    item.addEventListener('click', () =>
+        item.classList.toggle('catalog__products-like_clicked'));
+});
+
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider__button--next',
+      prevEl: '.slider__button--prev',
+    },
+  });
